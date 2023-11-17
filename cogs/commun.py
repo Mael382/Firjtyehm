fdaeff8dbd5a8c1b83d8a75e37d0de95edc7d9bc
Wael382 <97806796+Wael382@users.noreply.github.com>
2023-11-17 17:37:42 +0100

    Update commun.py

--- cogs/commun.py
+++ cogs/commun.py
@@ -3,13 +3,15 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-class Commun(commands.Cog):
-    def __init__(self, bot):
+
+class LynkrToCommun(commands.Cog):
+    def __init__(self, bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name = "commun", description = "Traduit le texte en Commun")
-    async def commun_slash(self, interaction: discord.Interaction, text: str):
-        await interaction.response.send_message("Cette fonctionnalité n'est pas encore disponible, merci de bien vouloir patienter !", ephemeral = True)
+    @app_commands.command(name = "communlynkr", description = "Traduit en Commun, un texte écrit en Lynkr")
+    async def lynkr_to_commun_slash(self, interaction: discord.Interaction, text: str):
+        await interaction.response.send_message("Cette fonctionnalité est en cours de développement, merci de bien vouloir patienter !", ephemeral = True)
 
-async def setup(bot):
+
+async def setup(bot) -> None:
     await bot.add_cog(Commun(bot))
