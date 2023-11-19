fe62988ec18807931b63957f14c134d04e4ce27f
Wael382 <97806796+Wael382@users.noreply.github.com>
2023-11-19 19:25:12 +0100

    Update lynkr.py

--- cogs/lynkr.py
+++ cogs/lynkr.py
@@ -5,18 +5,28 @@ from discord.ext import commands
 from discord.utils import get
 
 import spacy
+import pandas as pd
 from text_to_num import text2num
 # from num2words import num2words
-import pandas as pd
-from typing import List, Dict, Tuple
 
-nlp = spacy.load("fr_core_news_md")
-# nlp = spacy.load("fr_dep_news_trf")
-df_ANP = pd.read_csv("./codex/lynkrANP.csv").astype(pd.StringDtype(storage = "pyarrow"))
-df_VER = pd.read_csv("./codex/lynkrVER.csv").astype(pd.StringDtype(storage = "pyarrow"))
-# df_NUM = pd.read_csv("./codex/lynkrNUM.csv").astype(pd.StringDtype(storage = "pyarrow"))
-df_ALL = pd.read_csv("./codex/lynkrALL.csv").astype(pd.StringDtype(storage = "pyarrow"))
-# Faire la chasse aux ajectifs possessifs
+# DEPRECATED
+# from typing import List, Dict, Tuple
+
+
+ANP_PATH = "./codex/lynkrANP.csv"
+VER_PATH = "./codex/lynkrVER.csv"
+NUM_PATH = "./codex/lynkrNUM.csv"
+ALL_PATH = "./codex/lynkrALL.csv"
+
+NLP = spacy.load("fr_dep_news_trf")
+
+ANP_SERIES = pd.read_csv(ANP_PATH).astype(pd.StringDtype(storage = "pyarrow")).set_index("lemma").squeeze()
+VER_SERIES = pd.read_csv(VER_PATH).astype(pd.StringDtype(storage = "pyarrow")).set_index("lemma").squeeze()
+# NUM_SERIES = pd.read_csv(NUM_PATH).astype(pd.StringDtype(storage = "pyarrow")).set_index("lemma").squeeze()
+NUM_SERIES = None # Le CSV doit encore être réalisé !
+ALL_SERIES = pd.read_csv(ALL_PATH).astype(pd.StringDtype(storage = "pyarrow")).set_index("lemma").squeeze()
+# Faire la chasse aux ajectifs possessifs (update : wtf, pourquoi j'ai écrit ça ?)
+
 
 # data = pd.read_csv("lynkr.csv").astype(pd.StringDtype(storage = "pyarrow"))
 # data = data.sort_values(by = "lemma", key = lambda col: col.str.normalize("NFKD").str.encode("ascii", errors = "ignore").str.decode("utf-8"))
@@ -50,27 +60,27 @@ def tokenize(text: str, nlp: spacy.lang.fr.French) -> list[dict[str, str | None]
     return tokens
 
 
-# deprecated
-def parse(text: str) -> List[Dict[str, str]]:
-    doc = nlp(text)
-    tokens = list()
+# DEPRECATED
+# def parse(text: str) -> List[Dict[str, str]]:
+#     doc = nlp(text)
+#     tokens = list()
 
-    for token in doc:
-        new_token = {"pos":token.pos_,"lemma": token.lemma_.lower(),
-                     "text": token.lower_, "shape": token.shape_, "number": None,
-                     "gender": None, "tense": None, "polarity": None}
-        morph = token.morph.to_dict()
-        if "Number" in morph:
-            new_token["number"] = morph["Number"]
-        if "Gender" in morph:
-            new_token["gender"] = morph["Gender"]
-        if "Tense" in morph:
-            new_token["tense"] = morph["Tense"]
-        if "Polarity" in morph:
-            new_token["polarity"] = morph["Polarity"]
-        tokens.append(new_token)
+#     for token in doc:
+#         new_token = {"pos":token.pos_,"lemma": token.lemma_.lower(),
+#                      "text": token.lower_, "shape": token.shape_, "number": None,
+#                      "gender": None, "tense": None, "polarity": None}
+#         morph = token.morph.to_dict()
+#         if "Number" in morph:
+#             new_token["number"] = morph["Number"]
+#         if "Gender" in morph:
+#             new_token["gender"] = morph["Gender"]
+#         if "Tense" in morph:
+#             new_token["tense"] = morph["Tense"]
+#         if "Polarity" in morph:
+#             new_token["polarity"] = morph["Polarity"]
+#         tokens.append(new_token)
 
-    return tokens
+#     return tokens
 
 
 def apply_case(token: dict[str, str | None], translated_token: str) -> str:
@@ -116,7 +126,29 @@ def translate_peut_etre(tokens: list[dict[str, str | None]]) -> str:
     return translated_tokens
 
 
-def translate_verbs(token: dict[str, str | None], negation: bool = False, series: pd.core.series.Series = VERBS_SERIES) -> str:
+def translate_neg(token: dict[str, str | None]) -> str:
+    translated_token = ""
+
+    return translated_token
+
+
+def translate_adj_noun_propn(token: dict[str, str | None], series: pd.core.series.Series = ANP_SERIES) -> str:
+    lemma = token["lemma"]
+
+    if lemma in series.index:
+        number = token["number"]
+        translated_token = series[lemma]
+        if (number == "Plur") and (translated_token[-1] != "s"):
+            translated_token += "s"
+        translated_token = apply_case(token, translated_token)
+    else:
+        text = token["text"]
+        translated_token = "**" + apply_case(token, text) + "**"
+
+    return translated_token
+
+
+def translate_verb_aux(token: dict[str, str | None], negation: bool, series: pd.core.series.Series = VER_SERIES) -> str:
     lemma = token["lemma"]
 
     if negation:
@@ -126,8 +158,10 @@ def translate_verbs(token: dict[str, str | None], negation: bool = False, series
 
     if lemma == "mourir":
         translated_token = prefix + "mortilem"
+        translated_token = apply_case(token, translated_token)
     elif lemma == "vivre":
         translated_token = prefix + "virvilem"
+        translated_token = apply_case(token, translated_token)
     elif lemma in series.index:
         tense = token["tense"]
         translated_token = prefix + series[lemma]
@@ -137,15 +171,16 @@ def translate_verbs(token: dict[str, str | None], negation: bool = False, series
             translated_token = translated_token[:-1] + "p"
         elif tense == "Fut":
             translated_token = translated_token[:-1] + "f"
+        translated_token = apply_case(token, translated_token)
     else:
         # Ajouter une fonctionnalitée de recherche de synonymes existants
         text = token["text"]
-        translated_token = "**" + text + "**"
+        translated_token = "**" + apply_case(token, text) + "**"
 
     return translated_token
 
 
-# deprecated
+# DEPRECATED
 def translate(tokens: List[Dict[str, str]]) -> Tuple[str, List[Dict[str, str]]]:
     # Ajouter nombres, ignorances des noms propres inconnus, "au revoir",
     # "race + sehr", "en-", "En-Bas", "peut-être", "quelque chose", "quelqu'un",
